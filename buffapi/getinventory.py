9e85c22498304a8015b83e752f42e14bf8e61576
Mert Dogan <mert.dogan@me.com>
2023-11-14 19:44:37 +0100

    getinv with multible lines

--- buffapi/getinventory.py
+++ buffapi/getinventory.py
@@ -1,3 +1,4 @@
+import time
 import requests
 import os
 import json
@@ -16,14 +17,32 @@ class Item:
 
 class Inventory:
     total_ammount = 0.0
+    total_page = 0
     items: list[Item] = []
 
-    def __init__(self, total_amount: float, items: list[Item]) -> None:
-        self.total_ammount = total_amount
-        self.items = items
-
-
 def getinv():
+    wait_time = 20
+
+    inv = Inventory()
+    inv = inv_addpage(inv, 1)
+    if inv is None:
+        return None
+
+    print("Waiting %dsec" % wait_time)
+    time.sleep(wait_time)
+
+    for i in range(2, inv.total_page + 1):
+        inv = inv_addpage(inv, i)
+        if inv is None:
+            return None
+
+        print("Waiting %dsec" % wait_time)
+        time.sleep(wait_time)
+
+    return inv
+    
+
+def inv_addpage(inv: Inventory, page: int):
     cookies = {
         'Locale-Supported': 'en',
         'game': 'csgo',
@@ -33,9 +52,7 @@ def getinv():
     headers = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'Accept-Language': 'en-US,en;q=0.5',
-        'X-Requested-With': 'XMLHttpRequest',
-        'DNT': '1',
-        'Referer': 'https://buff.163.com/market/steam_inventory?game=csgo',
+        'X-Requested-With': 'XMLHttpRequest', 'DNT': '1', 'Referer': 'https://buff.163.com/market/steam_inventory?game=csgo',
         'Sec-Fetch-Dest': 'empty',
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'same-origin',
@@ -45,7 +62,7 @@ def getinv():
     params = {
         'game': 'csgo',
         'force': '0',
-        'page_num': '1',
+        'page_num': str(page),
         'page_size': '50',
         'search': '',
         'state': 'all',
@@ -61,17 +78,18 @@ def getinv():
     try:
         data = response.json()["data"]
 
-        total_ammount = float(data["total_amount"])
-        items: list[Item] = []
+        inv.total_ammount = float(data["total_amount"])
+        inv.total_page = int(data["total_page"])
 
         for item in data["items"]:
             itemid = int(item["goods_id"])
             name = str(item["name"])
             price = float(item["sell_min_price"])
 
-            items.append(Item(itemid, name, price))
+            inv.items.append(Item(itemid, name, price))
 
-        return Inventory(total_ammount, items)
+        print("Page %d added to the inventory data" % page)
+        return inv
 
     except (KeyError, IndexError):
         print("Error while getting the data from the inventory")
