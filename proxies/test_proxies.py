9148e9013c530b40fa4a8e825cc13cfbbeeebf05
Mert Dogan <mert.dogan@me.com>
2023-11-21 01:50:27 +0100

    test_proxies timeout added

--- proxies/test_proxies.py
+++ proxies/test_proxies.py
@@ -16,11 +16,11 @@ def test_proxies(proxies: list[Proxy]):
 
         try:
             if proxy.name is None or proxy.password is None:
-                requests.get("http://google.com", proxies=proxy_server)
+                requests.get("http://google.com", proxies=proxy_server, timeout=5)
             else:
                 auth = (proxy.name, proxy.address)
                 requests.get("http://google.com",
-                             proxies=proxy_server, auth=auth)
+                             proxies=proxy_server, auth=auth, timeout=5)
 
         except KeyboardInterrupt:
             sys.exit(0)
